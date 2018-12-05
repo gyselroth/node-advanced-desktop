5f6c1209533bdb6d1f2dbedf71b2f00d3082933e
raffis <sahli@gyselroth.com>
2018-12-05 17:06:31 +0100

    tests

--- tests/index.js
+++ tests/index.js
@@ -5,6 +5,8 @@ sidebar.getItems().then((result) => {
   console.log(result);
 });
 
-sidebar.addItem('/Users/testuser/Balloon');
+sidebar.addItem('C:\Users\Raffael\Balloon\build', {
+  clsId: "05C645AA-6AA4-406B-B033-9142E21ECC5F"
+});
 
 nodeIcon.setFolderIcon('/Users/testuser/Balloon/test', '/Users/testuser/dev/balloon-client-desktop/resources/diricon/icon.png').catch((err) => {console.log(err);})
